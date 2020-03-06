4d1c0386b521663936e91d5be3dc26c570c8525b
Petr Drastil <petr.drastil@gmail.com>
2020-03-06 10:49:24 +0100

    Fix inventory for AWS Golden AMI

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "app_version" {
+  type        = string
+  default     = "v1.0"
+  description = "Version of deployed application"
+}
+
 variable "aws_region" {
   type        = string
   default     = "eu-central-1"
@@ -14,12 +20,6 @@ variable "network_cidr" {
   type        = string
   default     = "10.0.0.0/16"
   description = "VPC CIDR for deployment environment"
-}
-
-variable "app_version" {
-  type        = string
-  default     = "1.0"
-  description = "Version of deployed application"
 }
 
 variable "public_key_path" {
